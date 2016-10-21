db60fe2c47c0a0979ff67a6d63f0e023a232ca97
Alex <quixoticrocket@gmail.com>
2016-10-21 11:34:40 +0100

    simple file logging code in place

--- RateCalculator/SimpleFileLogger/FileLogger.cs
+++ RateCalculator/SimpleFileLogger/FileLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,24 +9,41 @@ namespace SimpleFileLogger
 {
     public class FileLogger : Shared.Interfaces.ILogger
     {
+        
+       //super simple logging... it leaves much to be desired, but this is not the focus of what we're doing.
+
         public void Critical(string message)
         {
-            throw new NotImplementedException();
+            using (StreamWriter filelog = new StreamWriter(".\\log.txt", true))
+            {
+                filelog.WriteLine(string.Format("{0} :: CRITICAL :: {1}", DateTime.Now.ToString(), message));
+            }
         }
 
         public void Debug(string message)
         {
-            throw new NotImplementedException();
+#if(DEBUG)
+            using (StreamWriter filelog = new StreamWriter(".\\log.txt", true))
+            {
+                filelog.WriteLine(string.Format("{0} :: DEBUG :: {1}", DateTime.Now.ToString(), message));
+            }
+#endif
         }
 
         public void Fatal(string message)
         {
-            throw new NotImplementedException();
+            using (StreamWriter filelog = new StreamWriter(".\\log.txt", true))
+            {
+                filelog.WriteLine(string.Format("{0} :: FATAL :: {1}", DateTime.Now.ToString(), message));
+            }
         }
 
         public void Information(string message)
         {
-            throw new NotImplementedException();
+            using (StreamWriter filelog = new StreamWriter(".\\log.txt", true))
+            {
+                filelog.WriteLine(string.Format("{0} :: INFORMATION :: {1}", DateTime.Now.ToString(), message));
+            }
         }
     }
 }
